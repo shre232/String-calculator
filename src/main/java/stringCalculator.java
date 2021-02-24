603f11bf8eef14bad1ce6a4543064612f67a0953
Shre232 <shreyasgaikwad232.1997@gmail.com>
2021-02-25 02:51:51 +0530

    Solution of 2nd problem

--- src/main/java/stringCalculator.java
+++ src/main/java/stringCalculator.java
@@ -1,18 +1,35 @@
+
 public class stringCalculator {
 
-    public static int Add(String numbers) {
-        String[] num = numbers.split(",|\n");
+    String[] num= {};
+    public  int Add(String numbers) {
         if (numbers.isEmpty()) {
             return 0;
         }
-        if (numbers.length() == 1) {
-            return Integer.parseInt(numbers);
-        } else {
-            int sum=0;
-            for (int i=0;i<num.length;i++){
-                sum+=Integer.parseInt(num[i]);
-            }
-            return sum;
 
-        }
-    }}
+
+//            if(numbers.charAt(0) == '\\') {
+//              char delimitor = numbers.charAt(2);
+//
+//               num = numbers.substring(4).split("delimiter|\n");
+//               return Integer.parseInt(numbers);     }
+//           else {
+                num = numbers.split(",|\n");
+//           }
+
+
+            if (numbers.length() == 1) {
+                return Integer.parseInt(numbers);}
+              else{  int sum = 0;
+                for (int i = 0; i < num.length; i++) {
+
+                    sum += Integer.parseInt(num[i]);
+                }
+                return sum;
+            }
+
+
+
+
+
+        }}
--- src/test/java/testCalculator.java
+++ src/test/java/testCalculator.java
@@ -2,30 +2,40 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 class testCalculate {
+    stringCalculator stringcalculator=new stringCalculator();
+
     @Test
     public void emptyStringReturnZero() {
-        Assertions.assertEquals(stringCalculator.Add(""), 0);
+        stringCalculator stringcalculator=new stringCalculator();
+        Assertions.assertEquals(stringcalculator.Add(""), 0);
     }
 
     @Test
     public void number_for_one_number(){
-        Assertions.assertEquals(stringCalculator.Add("1") ,1);
+        Assertions.assertEquals(stringcalculator.Add("1") ,1);
     }
     @Test
     public void sum_of_two_numbers() {
-        Assertions.assertEquals(stringCalculator.Add("1,2"), 3);
+        Assertions.assertEquals(stringcalculator.Add("1,2"), 3);
     }
     @Test
     public void sum_of_unknown_numbers() {
-        Assertions.assertEquals(stringCalculator.Add("1,2,3"), 6);
+        Assertions.assertEquals(stringcalculator.Add("1,2,3"), 6);
     }
     @Test
     public void sum_of_two_with_newline_delimeter_numbers(){
-        Assertions.assertEquals(stringCalculator.Add( "1\n2"),3);
+        Assertions.assertEquals(stringcalculator.Add( "1\n2"),3);
     }
+//    @Test
+//    public void supporting_different_delimiters(){
+//        Assertions.assertEquals(stringcalculator.Add("//;\n1;2"),3);
+
+   }
+//    @Test
+//    public void Exeption_for_comma(){
+//        Assertions.(()->stringcalculator.Add("1,\n2"))
+//                .isInstanceOf(NumberFormatException);
 
 
 
 
-
-}
